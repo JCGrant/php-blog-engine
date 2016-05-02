c25a73ba08604e8fee9320dd9f2e1530a00ee72e
JCGrant <jamescolin.grant@gmail.com>
2016-05-02 10:00:43 +0100

    If $post_id is not an int, redirect to 404.

--- app/routes.php
+++ app/routes.php
@@ -14,14 +14,15 @@ $app->get('/blog', function($request, $response, $args) {
   return $this->view->render($response, 'blog.html', ['posts' => $posts]);
 })->setName('blog');
 
-$app->get('/blog/post/{id}', function($request, $response, $args) {
+$app->get('/blog/post/{id:[0-9]+}', function($request, $response, $args) {
   $post_id = $args['id'];
   $sql = "SELECT id, title, content, datetime " .
          "FROM post " .
          "WHERE id=$post_id";
   $stmt = $this->db->query($sql);
   if (!$row = $stmt->fetch()) {
-    return $response->withStatus(302)->withHeader('Location', '/404');  }
+    return $response->withStatus(302)->withHeader('Location', '/404');
+  }
   $post = new Post($row);
   return $this->view->render($response, 'post.html', ['post' => $post]);
 })->setName('post');
