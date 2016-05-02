66084c751dce6381f711b49a7ca2a326882602d1
JCGrant <jamescolin.grant@gmail.com>
2016-05-02 15:55:53 +0100

    Refactor app.js.

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,22 @@
-datetimes = document.getElementsByClassName('datetime');
-Array.prototype.forEach.call(datetimes, function(datetime) {
-  time_str = datetime.innerHTML;
-  date = new Date(time_str);
-  datetime.innerHTML = date.toDateString();
-});
-
-var posts = document.getElementsByClassName('post');
-Array.prototype.forEach.call(posts, function(post) {
-  var content = post.getElementsByClassName('content')[0];
+function formatPostContent(content) {
   var paragraphs = content.innerHTML.split('\n\n');
   var formatted_html = '';
   paragraphs.forEach(function(paragraph) {
     formatted_html += '<p>' + paragraph + '</p>';
   });
   content.innerHTML = formatted_html;
+}
+
+function formatPostDatetime(datetime) {
+  date = new Date(datetime.innerHTML);
+  datetime.innerHTML = date.toDateString();
+}
+
+var posts = document.getElementsByClassName('post');
+Array.prototype.forEach.call(posts, function(post) {
+  var content = post.getElementsByClassName('content')[0];
+  formatPostContent(content);
+
+  var datetime = post.getElementsByClassName('datetime')[0];
+  formatPostDatetime(datetime);
 });
