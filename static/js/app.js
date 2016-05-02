47bffd790054f586080f4dc49012c1d7b809da02
JCGrant <jamescolin.grant@gmail.com>
2016-05-02 15:40:23 +0100

    Format posts with Javascript.

--- static/js/app.js
+++ static/js/app.js
@@ -4,3 +4,14 @@ Array.prototype.forEach.call(datetimes, function(datetime) {
   date = new Date(time_str);
   datetime.innerHTML = date.toDateString();
 });
+
+var posts = document.getElementsByClassName('post');
+Array.prototype.forEach.call(posts, function(post) {
+  var content = post.getElementsByClassName('content')[0];
+  var paragraphs = content.innerHTML.split('\n\n');
+  var formatted_html = '';
+  paragraphs.forEach(function(paragraph) {
+    formatted_html += '<p>' + paragraph + '</p>';
+  });
+  content.innerHTML = formatted_html;
+});
